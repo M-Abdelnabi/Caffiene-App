466f31e3cd0e64bf262788ebb80850dbee7b58d6
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-08 01:31:15 +0300

    add drawables,colors and fonts

--- app/src/main/java/com/example/caffiene_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/caffiene_app/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val LightGrayBackground = Color(0xFFF5F5F5)
+val DarkGray = Color(0xFF3B3B3B)
+val Gray = Color(0xFFB3B3B3)
+val SoftPeach = Color(0xFFFFEEE7)
+val DeepBrown = Color(0xFF7C351B)
+val DarkChocolate = Color(0xFF291710)
+val MidnightBlack = Color(0xFF030004)
+val Charcoal = Color(0xFF1F1F1F)
+val SemiTransparentWhite = Color(0xDEFFFFFF)
+
