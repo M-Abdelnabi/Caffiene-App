b1abf757f0d63d8fd5f1ea4f00b08ccad65406a6
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-08 01:46:09 +0300

    add home top bar icons

---
+++ app/src/main/java/com/example/caffiene_app/composables/HomeTopBar.kt
@@ -0,0 +1,63 @@
+package com.example.caffiene_app.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.caffiene_app.R
+import com.example.caffiene_app.ui.theme.LightGrayBackground
+
+@Composable
+fun HomeTopBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            modifier = modifier
+                .size(48.dp, 48.dp)
+                .clip(CircleShape)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_ghost),
+                contentDescription = "",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        Box(
+            modifier = modifier
+                .size(48.dp, 48.dp)
+                .clip(CircleShape)
+                .background(LightGrayBackground)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_plus),
+                contentDescription = "",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeTopBarPreview() {
+    HomeTopBar()
+}
